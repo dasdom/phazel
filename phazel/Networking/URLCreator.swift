efa3086eece3c01be2f1b521ee13885d01319bae
Dominik Hauser <hauser@energy-app-provider.com>
2017-01-25 17:51:20 +0100

    Improved tests

--- phazel/Networking/URLCreator.swift
+++ phazel/Networking/URLCreator.swift
@@ -9,17 +9,19 @@
 import Foundation
 
 enum URLCreator {
-  case auth(username: String, password: String)
-  
-  func url() -> URL? {
-    switch self {
-    case .auth(let username, let password):
-      let urlString = "\(self.baseURLString)oauth/access_token?username=\(username)&password=\(password)"
-      return URL(string: urlString)
+    case auth(username: String, password: String)
+    
+    func url() -> URL? {
+        switch self {
+        case .auth(let username, let password):
+            guard let encodedUsername = username.addingPercentEncoding(withAllowedCharacters: CharacterSet(charactersIn: "/%&=?$#+-~@<>|\\*,.()[]{}^!").inverted), let encodedPassword = password.addingPercentEncoding(withAllowedCharacters: CharacterSet(charactersIn: "/%&=?$#+-~@<>|\\*,.()[]{}^!").inverted) else { fatalError() }
+            let urlString = "\(self.baseURLString)oauth/access_token?username=\(encodedUsername)&password=\(encodedPassword)"
+            print(urlString)
+            return URL(string: urlString)
+        }
     }
-  }
-  
-  var baseURLString: String {
-    return "https://api.pnut.io/v0/"
-  }
+    
+    var baseURLString: String {
+        return "https://api.pnut.io/v0/"
+    }
 }
--- phazelTests/Networking/URLCreatorTests.swift
+++ phazelTests/Networking/URLCreatorTests.swift
@@ -10,25 +10,28 @@ import XCTest
 @testable import phazel
 
 class URLCreatorTests: XCTestCase {
-  
-  override func setUp() {
-    super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-  }
-  
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    super.tearDown()
-  }
-  
-  func test_authURL() {
-    guard let url = URLCreator.auth(username: "foo", password:"bar").url() else { return XCTFail() }
     
-    //XCTAssertEqual(url, URL(string: "https://api.pnut.io/v0/oauth/access_token?client_id=42&password_grant_secret=23&username=foo&"))
-    let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
-    XCTAssertEqual(urlComponents?.host, "api.pnut.io")
-    XCTAssertEqual(urlComponents?.path, "/v0/oauth/access_token")
-    XCTAssertTrue((urlComponents?.queryItems?.contains(URLQueryItem(name: "username", value: "foo"))) ?? false)
-    XCTAssertTrue((urlComponents?.queryItems?.contains(URLQueryItem(name: "password", value: "bar"))) ?? false)
-  }
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_authURL() {
+        let username = "föö"
+        let password = "@#$%^&*\\/?"
+        guard let url = URLCreator.auth(username: username, password:password).url() else { return XCTFail() }
+        
+        //XCTAssertEqual(url, URL(string: "https://api.pnut.io/v0/oauth/access_token?client_id=42&password_grant_secret=23&username=foo&"))
+        let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        XCTAssertEqual(urlComponents?.host, "api.pnut.io")
+        XCTAssertEqual(urlComponents?.path, "/v0/oauth/access_token")
+        let queryIems = urlComponents?.queryItems
+        XCTAssertTrue((queryIems?.contains(URLQueryItem(name: "username", value: username))) ?? false, "Found queryItems: \(queryIems)")
+        XCTAssertTrue((queryIems?.contains(URLQueryItem(name: "password", value: password))) ?? false, "Found queryItems: \(queryIems)")
+    }
 }
