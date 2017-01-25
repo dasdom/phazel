23d9339ae290dd556520f4797b93e2cbf4c014bb
Dominik Hauser <dom@dasdom.de>
2017-01-25 20:49:41 +0100

    Add gitignore

--- phazel/Networking/URLCreator.swift
+++ phazel/Networking/URLCreator.swift
@@ -1,7 +1,3 @@
-//
-//  URLCreator.swift
-//  phazel
-//
 //  Created by dasdom on 24/01/2017.
 //  Copyright © 2017 dasdom. All rights reserved.
 //
@@ -14,14 +10,19 @@ enum URLCreator {
     func url() -> URL? {
         switch self {
         case .auth(let username, let password):
-            guard let encodedUsername = username.addingPercentEncoding(withAllowedCharacters: CharacterSet(charactersIn: "/%&=?$#+-~@<>|\\*,.()[]{}^!").inverted), let encodedPassword = password.addingPercentEncoding(withAllowedCharacters: CharacterSet(charactersIn: "/%&=?$#+-~@<>|\\*,.()[]{}^!").inverted) else { fatalError() }
-            let urlString = "\(self.baseURLString)oauth/access_token?username=\(encodedUsername)&password=\(encodedPassword)"
+            guard let encodedUsername = username.addingPercentEncoding(withAllowedCharacters: queryItemAllowedCharacterSet),
+                let encodedPassword = password.addingPercentEncoding(withAllowedCharacters: queryItemAllowedCharacterSet) else { fatalError() }
+            let urlString = "\(baseURLString)oauth/access_token?username=\(encodedUsername)&password=\(encodedPassword)"
             print(urlString)
             return URL(string: urlString)
         }
     }
     
-    var baseURLString: String {
+    private var baseURLString: String {
         return "https://api.pnut.io/v0/"
+    }
+    
+    private var queryItemAllowedCharacterSet: CharacterSet {
+        return CharacterSet(charactersIn: "/%&=?$#+-~@<>|\\*,.()[]{}^!").inverted
     }
 }
